0af36d17043175f45a6a4a7722e666c88d3031f4
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2019-09-30 21:04:22 +0530

    #241 AudienceLevel should support the new data format

--- DevoxxClientMobile/src/main/java/com/devoxx/views/SessionPresenter.java
+++ DevoxxClientMobile/src/main/java/com/devoxx/views/SessionPresenter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2018 Gluon Software
+ * Copyright (c) 2016, 2019 Gluon Software
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
@@ -28,6 +28,7 @@ package com.devoxx.views;
 import com.devoxx.DevoxxApplication;
 import com.devoxx.DevoxxView;
 import com.devoxx.control.DataLabel;
+import com.devoxx.model.AudienceLevel;
 import com.devoxx.model.Link;
 import com.devoxx.model.Session;
 import com.devoxx.model.Speaker;
@@ -365,13 +366,7 @@ public class SessionPresenter extends GluonPresenter<DevoxxApplication> {
     }
 
     private Label fetchLabelForAudienceLevel(String audienceLevel) {
-        if (audienceLevel.equalsIgnoreCase("L1")) {
-            return new Label(DevoxxBundle.getString("OTN.SESSION.AUDIENCE_LEVEL.BEGINNER"));
-        } else if (audienceLevel.equalsIgnoreCase("L2")) {
-            return new Label(DevoxxBundle.getString("OTN.SESSION.AUDIENCE_LEVEL.INTERMEDIATE"));
-        } else {
-            return new Label(DevoxxBundle.getString("OTN.SESSION.AUDIENCE_LEVEL.EXPERT"));
-        }
+        return new Label(AudienceLevel.valueOf(audienceLevel).getText());
     }
 
     private Label createTag(String tagText) {
