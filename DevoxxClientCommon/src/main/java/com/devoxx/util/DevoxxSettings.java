d94ffb0878c8e5169c8775a3aa87075916e48b90
Gluon Jenkins <jenkins@gluonhq.com>
2019-10-01 13:55:14 +0200

    new build: 386

--- DevoxxClientCommon/src/main/java/com/devoxx/util/DevoxxSettings.java
+++ DevoxxClientCommon/src/main/java/com/devoxx/util/DevoxxSettings.java
@@ -34,7 +34,7 @@ import java.time.format.FormatStyle;
 import java.util.*;
 
 public class DevoxxSettings {
-    public static final String BUILD_NUMBER = "385"                                                    ;
+    public static final String BUILD_NUMBER = "386"                                                     ;
 
     /**
      * boolean option to switch on/off automatic authentication by using a self generated random UUID
